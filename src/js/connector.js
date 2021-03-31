ec97191ab4b568b7780ce94b60a499cf6bf8a556
oibafs <fabioscaravelli@gmail.com>
2021-03-31 19:43:49 -0300

    First button test

---
+++ src/js/connector.js
@@ -0,0 +1,16 @@
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
+
+window.TrelloPowerUp.initialize({
+  'card-buttons': function (t, opts) {
+    return [{
+      // usually you will provide a callback function to be run on button click
+      // we recommend that you use a popup on click generally
+      icon: GRAY_ICON, // don't use a colored icon here
+      text: 'Postpone',
+      callback: onBtnClick,
+      condition: 'edit'
+    }];
+  }
+});
