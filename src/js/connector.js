3f185accfa44d2c1639cc7c66886cec8a244e6f0
oibafs <fabioscaravelli@gmail.com>
2021-03-31 20:18:41 -0300

    Wrong URL

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,7 +1,7 @@
 var onBtnClick = function (t, opts) {
   return t.card('id')
     .then(function(card) {
-      fetch(`https://https://scheduler-ruby.vercel.app/api/1/trello/cards/${card.id}/postpone`)
+      fetch(`https://scheduler-ruby.vercel.app/api/1/trello/cards/${card.id}/postpone`)
         .then(function(response) {
           return response.json();
         })
