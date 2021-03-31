0394b3f60915e825479107b819da9012aa850c32
oibafs <fabioscaravelli@gmail.com>
2021-03-31 19:58:15 -0300

    Deactivate reference to GRAY_ICON

--- src/js/connector.js
+++ src/js/connector.js
@@ -7,7 +7,7 @@ window.TrelloPowerUp.initialize({
     return [{
       // usually you will provide a callback function to be run on button click
       // we recommend that you use a popup on click generally
-      icon: GRAY_ICON, // don't use a colored icon here
+      // icon: GRAY_ICON, // don't use a colored icon here
       text: 'Postpone',
       callback: onBtnClick,
       condition: 'edit'
